309d65b3aa7797f3fa02585e2050a50fa1c8feca
pochapsky <131448204+pochapsky@users.noreply.github.com>
2023-10-22 15:07:50 +0300

    di-annotation

---
+++ src/main/java/ru/netology/repository/PostRepository.java
@@ -0,0 +1,43 @@
+package ru.netology.repository;
+
+import org.springframework.stereotype.Repository;
+import ru.netology.model.Post;
+
+import java.util.*;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.atomic.AtomicLong;
+
+@Repository
+public class PostRepository {
+    private final AtomicLong postId;
+    private final Map<Long, Post> postsMap;
+
+    public PostRepository() {
+        postId = new AtomicLong(0);
+        postsMap = new ConcurrentHashMap<>();
+    }
+
+    public List<Post> all() {
+        return new ArrayList<>(postsMap.values());
+    }
+
+    public Optional<Post> getById(long id) {
+        return Optional.ofNullable(postsMap.get(id));
+    }
+
+    public Post save(Post post) {
+        long specificID = post.getId();
+        if (specificID > 0 && postsMap.containsKey(specificID)) {
+            postsMap.replace(specificID, post);
+        } else {
+            long newIdPost = specificID == 0 ? postId.incrementAndGet() : specificID;
+            post.setId(newIdPost);
+            postsMap.put(newIdPost, post);
+        }
+        return post;
+    }
+
+    public void removeById(long id) {
+        postsMap.remove(id);
+    }
+}
